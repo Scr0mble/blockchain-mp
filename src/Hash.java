2a7b50527187eff8978d7ebaacb28793cb9f11a0
William <pitchfor@grinnell.edu>
2024-04-06 16:11:03 -0500

    added a few things to block and hash

--- src/Hash.java
+++ src/Hash.java
@@ -1,3 +1,18 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Hash {
-  
+  /* Fields */
+
+  byte[] curhash;
+
+  /* Constructors */
+
+  public Hash(byte[] data) {
+    this.curhash = data;
+  }
+
+  /* Methods */
+
+  byte
 }
